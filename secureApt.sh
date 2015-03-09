5844f465db96cccabba8ef0abf2f96befa39972b
philicious <the.cypher@gmail.com>
2015-03-09 16:06:29 +0100

    now secureApt should work

--- secureApt.sh
+++ secureApt.sh
@@ -14,7 +14,7 @@ Version: 12.04
 Codename: precise
 Date: ${DATE}
 Architectures: amd64
-Components: main
+Components: repodata
 Description: Ubuntu Precise 12.04
 MD5Sum:"
 
@@ -22,11 +22,17 @@ PACKAGES_MD5=($(md5sum Packages))
 PACKAGES_SIZE=$(stat -c%s Packages)
 PACKAGESGZ_MD5=($(md5sum Packages.gz))
 PACKAGESGZ_SIZE=$(stat -c%s Packages.gz)
+PACKAGES_SHA256=($(sha256sum Packages))
+PACKAGESGZ_SHA256=($(sha256sum Packages.gz))
 
 # write Release file with MD5s
 rm -rf Release
-echo -e "${HEADER}\n${PACKAGES_MD5}\t${PACKAGES_SIZE}\tPackages" > Release
-echo -e "${PACKAGESGZ_MD5}\t${PACKAGESGZ_SIZE}\tPackages.gz" >> Release
+echo -e "${HEADER}" > Release
+echo -e " ${PACKAGES_MD5}\t${PACKAGES_SIZE}\trepodata/binary-amd64/Packages" >> Release
+echo -e " ${PACKAGESGZ_MD5}\t${PACKAGESGZ_SIZE}\trepodata/binary-amd64/Packages.gz" >> Release
+echo -e "SHA256:" >> Release
+echo -e " ${PACKAGES_SHA256}\t${PACKAGES_SIZE}\trepodata/binary-amd64/Packages" >> Release
+echo -e " ${PACKAGESGZ_SHA256}\t${PACKAGESGZ_SIZE}\trepodata/binary-amd64/Packages.gz" >> Release
 
 # write the signature for Release file
 rm -rf Release.gpg
