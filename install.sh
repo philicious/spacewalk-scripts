f62ee0a0bfac4ce164036f9bb7c1c2ac48394737
liberodark <liberodark@gmail.com>
2019-04-19 10:01:39 +0200

    add installer

---
+++ install.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#
+# About: Install Script of Spacewalk automatically
+# Author: liberodark
+# License: GNU GPLv3
+
+#=================================================
+# CHECK UPDATE
+#=================================================
+
+  update_source="https://raw.githubusercontent.com/liberodark/spacewalk-scripts/master/install.sh"
+  version="0.0.1"
+
+  echo "Welcome on Spacewalk Script Install $version"
+
+  # make update if asked
+  if [ "$1" = "noupdate" ]; then
+    update_status="false"
+  else
+    update_status="true"
+  fi ;
+
+  # update updater
+  if [ "$update_status" = "true" ]; then
+    wget -O $0 $update_source
+    $0 noupdate
+    exit 0
+fi ;
+
+#=================================================
+# CHECK ROOT
+#=================================================
+
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
+
+#=================================================
+# IP
+#=================================================
+
+echo "What is your user of spacewalk ?"
+read user
+
+echo "What is your password of spacewalk ?"
+read password
+
+#=================================================
+# RETRIEVE ARGUMENTS FROM THE MANIFEST AND VAR
+#=================================================
+
+distribution=$(cat /etc/*release | head -n +1 | awk '{print $1}')
+
+#==============================================
+# SPACEWALK
+#==============================================
+echo "Install Script for Spacewalk"
+
+# Check OS & spacewalk
+
+  if [ $? != 1 ]; then
+
+    if [[ "$distribution" =~ .CentOS || "$distribution" = CentOS ]]; then
+      yum install -y html2text
+      cd centos/
+      cp -a spacewalk_sync_debian.cron /etc/cron.daily/spacewalk_sync_debian.cron
+      sed -i "s@MYLOGIN@${user}@@g" errata-import-debian.py
+      sed -i "s@MYPASSWORD@${password}@@g" errata-import-debian.py
+      mkdir -p /home/errata/spacewalk-scripts/
+      mv *.py /home/errata/spacewalk-scripts/
+      
+    fi
+fi
